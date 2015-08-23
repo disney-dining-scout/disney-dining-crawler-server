8c3fef669b9079a146a4de89676442796292387e
Matthew Voss <mattv@tamu.edu>
2015-08-23 14:34:42 -0500

    break out uids

--- server.js
+++ server.js
@@ -48,15 +48,9 @@
               subChannel = _channel[1];
           console.log("channel ", channel, ": ", message);
           message = JSON.parse(message);
-          if (subChannel === "getsearch") {
+          if (subChannel === "requestsearch") {
             //getSearch(message);
-            queue.push(
-              message,
-              function (err) {
-                console.log('finished processing search request', message.clientId);
-              }
-            );
-
+            processSearch(message);
           } else if (subChannel === "iplogupdate") {
             updateIpLog(message);
           }
@@ -86,235 +80,168 @@
           database        : config.get("mysql:database")
         });
 
-      };
+      },init = function() {
+        syslog.init("crawler", syslog.LOG_PID || syslog.LOG_ODELAY, syslog.LOG_LOCAL0);
+        syslog.log(syslog.LOG_INFO, "Server started");
+        if (process.argv[2]) {
+          if (fs.lstatSync(process.argv[2])) {
+              configFile = require(process.argv[2]);
+          } else {
+              configFile = process.cwd() + '/config/settings.json';
+          }
+        } else {
+          configFile = process.cwd()+'/config/settings.json';
+        }
 
-  var init = function() {
-    syslog.init("crawler", syslog.LOG_PID || syslog.LOG_ODELAY, syslog.LOG_LOCAL0);
-    syslog.log(syslog.LOG_INFO, "Server started");
-    if (process.argv[2]) {
-      if (fs.lstatSync(process.argv[2])) {
-          configFile = require(process.argv[2]);
-      } else {
-          configFile = process.cwd() + '/config/settings.json';
-      }
-    } else {
-      configFile = process.cwd()+'/config/settings.json';
-    }
+        refreshConfiguration();
 
-    refreshConfiguration();
+        connectDB();
 
-    connectDB();
+        latestUids = (config.get("uidBufferSize")) ? new CBuffer(config.get("uidBufferSize")) : new CBuffer(75);
 
-    latestUids = (config.get("uidBufferSize")) ? new CBuffer(config.get("uidBufferSize")) : new CBuffer(75);
-
-    subClient = redis.createClient(
-      config.get("redis:port"),
-      config.get("redis:host")
-    );
-    if (config.get("redis:db")) {
-      subClient.select(
-        config.get("redis:db"),
-        function() {
+        subClient = redis.createClient(
+          config.get("redis:port"),
+          config.get("redis:host")
+        );
+        if (config.get("redis:db")) {
+          subClient.select(
+            config.get("redis:db"),
+            function() {
+              setSubscription();
+            }
+          );
+        } else {
           setSubscription();
         }
-      );
-    } else {
-      setSubscription();
-    }
 
-    pubClient = redis.createClient(
-      config.get("redis:port"),
-      config.get("redis:host")
-    );
-    if (config.get("redis:db") >= 0) {
-      pubClient.select(
-        config.get("redis:db"),
-        function() {
-          //console.log("Redis DB set to:", config.get("redis:db"));
-        }
-      );
-    }
-    setTimeout(
-      function() {
-        sendReady();
-      },
-      3000
-    );
-
-    if (config.get("log")) {
-      var access_logfile = fs.createWriteStream(config.get("log"), {flags: 'a'});
-    }
-
-  };
-
-  queue = async.queue(
-    function (search, callback) {
-      console.log('queue length:', queue.length());
-      pool.getConnection(
-        function(err, connection) {
-          async.parallel(
-            {
-              searches: function(cb) {
-                var now = parseInt(moment().tz("America/New_York").format("H"), 10),
-                  offset = (now >= 3 && now < 6) ? "30" : "5",
-                  limit = config.get("limit") ? config.get("limit") : "10",
-                  typeOfSearch = (search.type === "paid") ? "IN" : "NOT IN",
-                  sql = "SELECT "+
-                        " globalSearches.*, "+
-                        " userSearches.restaurant, "+
-                        " userSearches.secondary, "+
-                        " restaurants.name, "+
-                        " secondary.name AS secondaryName, "+
-                        " userSearches.date, "+
-                        " userSearches.partySize "+
-                        "FROM globalSearches "+
-                        "JOIN userSearches ON globalSearches.uid = userSearches.uid "+
-                        "JOIN restaurants ON userSearches.restaurant = restaurants.id "+
-                        "LEFT JOIN restaurants AS secondary ON userSearches.secondary = secondary.id "+
-                        "WHERE userSearches.date >= UTC_TIMESTAMP() AND globalSearches.lastChecked < UTC_TIMESTAMP() - INTERVAL "+offset+" MINUTE "+
-                        " AND userSearches.date <= UTC_TIMESTAMP() + INTERVAL 180 DAY "+
-                        " AND userSearches.deleted = 0 AND userSearches.enabled = 1 AND globalSearches.deletedAt IS NULL " +
-                        " AND userSearches.user " + typeOfSearch + " (SELECT id FROM `users` WHERE subExpires >= UTC_TIMESTAMP())";
-                if (latestUids.toArray().length > 0) {
-                  var uids = latestUids.toArray().map(function(uid){
-                      // This will wrap each element of the uids array with quotes
-                      return "'" + uid + "'";
-                  }).join(",");
-                  sql += " AND globalSearches.uid NOT IN ("+uids+") ";
-                }
-                sql += "GROUP BY globalSearches.uid ";
-                sql += "ORDER BY globalSearches.lastChecked ASC ";
-                sql += "LIMIT " + search.number.toString();
-                if (subCounter > config.get("freeLimit")) {
-                  console.log('Using free searches:', typeOfSearch, subCounter);
-                  subCounter = 0;
-                }
-                subCounter += search.number;
-                //console.log(sql);
-                connection.query(
-                  sql,
-                  function(error, searches) {
-                    if (!error) {
-                      cb(null, searches);
-                    } else {
-                      cb(error);
-                    }
-                  }
-                );
-              },
-              ips: function(cb) {
-                var sql = "SELECT ipAddress.*, max.createdAt " +
-                          "FROM ipAddress " +
-                          "LEFT JOIN  ( " +
-                          "              SELECT ipAddressId, " +
-                          "                      MAX(createdAt) AS createdAt " +
-                          "              FROM ipAddressLog " +
-                          "              GROUP BY ipAddressId " +
-                          "            ) max ON (max.ipAddressId = ipAddress.id) " +
-                          "WHERE ipAddress.deletedAt IS NULL AND ipAddress.enabled = 1 " +
-                          "ORDER BY max.createdAt ASC, ipAddress.id ASC " +
-                          "LIMIT " + search.number.toString();
-                connection.query(
-                  sql,
-                  function(error, ips) {
-                    if (!error) {
-                      cb(null, ips);
-                    } else {
-                      cb(error);
-                    }
-                  }
-                );
-              },
-              agents: function(cb) {
-                var sql = "SELECT * " +
-                          "FROM userAgents, ( " +
-                          "        SELECT id AS sid " +
-                          "        FROM userAgents " +
-                          "        ORDER BY RAND( ) " +
-                          "        LIMIT ? " +
-                          "    ) tmp " +
-                          "WHERE userAgents.id = tmp.sid";
-                connection.query(
-                  sql,
-                  [search.number],
-                  function(error, agents) {
-                    if (!error) {
-                      cb(null, agents);
-                    } else {
-                      cb(error);
-                    }
-                  }
-                );
-              }
-            },
-            function(error, results) {
-              console.log(error);
-              if (!error) {
-                var i = 0,
-                    finished = function() {
-                      connection.release();
-                      callback();
-                    };
-                if (results.searches.length > 0 && results.ips.length > 0) {
-                  async.mapSeries(
-                    results.searches,
-                    function(search, cback) {
-                      var sql = "INSERT INTO ipAddressLog (ipAddressId, createdAt, updatedAt) values(?, UTC_TIMESTAMP(), UTC_TIMESTAMP())";
-                      search.ipAddress = {
-                        ip: results.ips[i].ipAddress,
-                        id: null
-                      };
-                      search.userAgent = results.agents[i].agent;
-                      pubClient.lpush('uids', search.uid);
-                      pubClient.ltrim('uids', 0, 500);
-                      latestUids.push(search.uid);
-                      connection.query(
-                        sql,
-                        [results.ips[i].id],
-                        function(error, res) {
-                          i++;
-                          if (!error) {
-                            search.ipAddress.id = res.insertId;
-                            cback(null, search);
-                          } else {
-                            console.log(error);
-                            cback(null, search);
-                          }
-                        }
-                      );
-
-                    },
-                    function(err, searches){
-                      var message = underscore.extend(search, {searches: searches});
-                      pubClient.publish("disneydining:sendsearch", JSON.stringify(message));
-                      finished();
-
-                    }
-                  );
-                } else if (results.searches.length > 0) {
-                  var message = underscore.extend(search, {searches: results.searches});
-                  pubClient.publish("disneydining:sendsearch", JSON.stringify(message));
-                  finished();
-                } else {
-                  finished();
-                }
-              }
+        pubClient = redis.createClient(
+          config.get("redis:port"),
+          config.get("redis:host")
+        );
+        if (config.get("redis:db") >= 0) {
+          pubClient.select(
+            config.get("redis:db"),
+            function() {
+              //console.log("Redis DB set to:", config.get("redis:db"));
             }
           );
         }
-      );
-    },
-    1
-  );
+        setTimeout(
+          function() {
+            sendReady();
+          },
+          3000
+        );
 
-  queue.drain = function() {
-    console.log('all searches have been processed');
+        if (config.get("log")) {
+          var access_logfile = fs.createWriteStream(config.get("log"), {flags: 'a'});
+        }
+
+      };
+
+  var processSearch = function (request) {
+    pool.getConnection(
+      function(err, connection) {
+        async.parallel(
+          {
+            ips: function(cb) {
+              var sql = "SELECT ipAddress.*, max.createdAt " +
+                        "FROM ipAddress " +
+                        "LEFT JOIN  ( " +
+                        "              SELECT ipAddressId, " +
+                        "                      MAX(createdAt) AS createdAt " +
+                        "              FROM ipAddressLog " +
+                        "              GROUP BY ipAddressId " +
+                        "            ) max ON (max.ipAddressId = ipAddress.id) " +
+                        "WHERE ipAddress.deletedAt IS NULL AND ipAddress.enabled = 1 " +
+                        "ORDER BY max.createdAt ASC, ipAddress.id ASC " +
+                        "LIMIT " + request.number.toString();
+              connection.query(
+                sql,
+                function(error, ips) {
+                  if (!error) {
+                    cb(null, ips);
+                  } else {
+                    cb(error);
+                  }
+                }
+              );
+            },
+            agents: function(cb) {
+              var sql = "SELECT * " +
+                        "FROM userAgents, ( " +
+                        "        SELECT id AS sid " +
+                        "        FROM userAgents " +
+                        "        ORDER BY RAND( ) " +
+                        "        LIMIT ? " +
+                        "    ) tmp " +
+                        "WHERE userAgents.id = tmp.sid";
+              connection.query(
+                sql,
+                [request.number],
+                function(error, agents) {
+                  if (!error) {
+                    cb(null, agents);
+                  } else {
+                    cb(error);
+                  }
+                }
+              );
+            }
+          },
+          function(error, results) {
+            console.log(error);
+            if (!error) {
+              var i = 0,
+                  finished = function() {
+                    connection.release();
+                    console.log('finished processing search request', request.clientId);
+                  };
+              if (request.searches.length > 0 && results.ips.length > 0) {
+                async.mapSeries(
+                  request.searches,
+                  function(search, cback) {
+                    var sql = "INSERT INTO ipAddressLog (ipAddressId, createdAt, updatedAt) values(?, UTC_TIMESTAMP(), UTC_TIMESTAMP())";
+                    search.ipAddress = {
+                      ip: results.ips[i].ipAddress,
+                      id: null
+                    };
+                    search.userAgent = results.agents[i].agent;
+                    connection.query(
+                      sql,
+                      [results.ips[i].id],
+                      function(error, res) {
+                        i++;
+                        if (!error) {
+                          search.ipAddress.id = res.insertId;
+                          cback(null, search);
+                        } else {
+                          console.log(error);
+                          cback(null, search);
+                        }
+                      }
+                    );
+
+                  },
+                  function(err, searches){
+                    var message = underscore.extend(request, {searches: searches});
+                    pubClient.publish("disneydining:sendsearch", JSON.stringify(message));
+                    finished();
+
+                  }
+                );
+              } else if (request.searches.length > 0) {
+                pubClient.publish("disneydining:sendsearch", JSON.stringify(request));
+                finished();
+              } else {
+                finished();
+              }
+            }
+          }
+        );
+      }
+    );
   };
-
-  queue.saturated = function() {
-    console.log('queue is saturated');
-  };
-
 
   init();
 
